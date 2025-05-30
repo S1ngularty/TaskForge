d11a3695667adaac738f06aba15b4440d608c3d2
S1ngularty <asherxd10245@gmail.com>
2025-05-30 23:04:08 +0800

    model eloquent relationship

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -1,10 +1,23 @@
 <?php
 
 namespace App\Models;
-
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    //
+    use HasFactory;
+
+    protected $primaryKey="task_id";
+    protected $fillable=[
+        'user_id',
+        'title',
+        'description',
+        'occurence'
+    ];
+
+    public function user(){
+        return $this->belongsTo(User::class,'user_id');
+    }
 }
